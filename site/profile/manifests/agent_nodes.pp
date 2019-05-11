1639cbbefcece12952df52a83b26692c46cac122
Mohamed Lotfy <36176128+mlotfy1@users.noreply.github.com>
2019-05-11 16:20:16 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,12 +2,14 @@ class profile::agent_nodes {
   include docker
   docker::run {'web.puppet.vm': 
       image     => 'puppet/puppet-agent',
-      command   => '/bin/bash -c "while true; do echo hello world; sleep 1; done"',
+      detach    => true,
+      command   => '/bin/bash',
       hostname  => 'web.puppet.vm',
   }
   docker::run {'db.puppet.vm': 
       image     => 'puppet/puppet-agent',
-      command   => '/bin/bash -c "while true; do echo hello world; sleep 1; done"',
+      detach    => true,
+      command   => '/bin/bash',
       hostname  => 'db.puppet.vm',
   }
-  
+}
