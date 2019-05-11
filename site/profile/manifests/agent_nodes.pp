85d3d0f25ed171575fc50e88a97f851c80819531
Mohamed Lotfy <36176128+mlotfy1@users.noreply.github.com>
2019-05-11 18:14:57 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,10 +2,12 @@ class profile::agent_nodes {
   include docker
   docker::run {'web.puppet.vm': 
       image     => 'puppet/puppet-agent',
+      links     => ['web.puppet.vm:master.puppet.vm'],
       hostname  => 'web.puppet.vm',
   }
   docker::run {'db.puppet.vm': 
       image     => 'puppet/puppet-agent',
+      links     => ['db.puppet.vm:master.puppet.vm'],
       hostname  => 'db.puppet.vm',
   }
 }
